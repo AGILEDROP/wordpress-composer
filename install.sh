c3ed4d847ca70e580dfba92745d4f3814a45a530
Ziga <ziga.koracin@agiledrop.com>
2020-05-29 12:33:54 +0200

    Update script for permission issue.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -o allexport
 [[ -f .env ]] && source .env
 set +o allexport
 
-docker-compose exec -u www-data web vendor/bin/wp config create --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASS} --dbhost=${WP_DB_HOST} --dbprefix=${WP_DB_PREFIX} --dbcharset=${WP_DB_CHARSET} --dbcollate=${WP_DB_COLLATE} --force
-docker-compose exec -u www-data web vendor/bin/wp core install --url=https://${PROJECT_HOSTNAME} --title=${PROJECT_NAME}
-docker-compose exec -u www-data web vendor/bin/wp theme activate wordpress-ad-theme
-docker-compose exec -u www-data web vendor/bin/wp rewrite flush
+docker-compose exec web vendor/bin/wp config create --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASS} --dbhost=${WP_DB_HOST} --dbprefix=${WP_DB_PREFIX} --dbcharset=${WP_DB_CHARSET} --dbcollate=${WP_DB_COLLATE} --force --allow-root
+docker-compose exec web vendor/bin/wp core install --url=https://${PROJECT_HOSTNAME} --title=${PROJECT_NAME} --allow-root
+docker-compose exec web vendor/bin/wp theme activate wordpress-ad-theme --allow-root
+docker-compose exec web vendor/bin/wp rewrite flush --allow-root
