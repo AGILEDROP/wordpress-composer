bba3e2bc49bdc19d3ca5912847ee42c77e9ba578
Ziga <ziga.koracin@agiledrop.com>
2020-06-05 09:09:22 +0200

    Check if dir exists.

--- composerScripts/postInstallPackage.sh
+++ composerScripts/postInstallPackage.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cd contrib/plugins
-installedPlugin=$(ls -ltr | tail -1 | grep -o '[^ ]*$')
-cd ..
-cd ..
-cp -ru contrib/plugins/$installedPlugin wp/wp-content/plugins/
+if [ -d "contrib/plugins" ] && [ -d "wp/wp-content/plugins" ]; then
+  cd contrib/plugins
+  installedPlugin=$(ls -ltr | tail -1 | grep -o '[^ ]*$')
+  cd ..
+  cd ..
+  cp -ru contrib/plugins/$installedPlugin wp/wp-content/plugins/
+fi
