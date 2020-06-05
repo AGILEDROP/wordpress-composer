71aaa577527b7bbc3f9f69401094a57637a16a10
Ziga <ziga.koracin@agiledrop.com>
2020-06-05 09:16:54 +0200

    Composer require for themes.

--- composerScripts/postInstallPackage.sh
+++ composerScripts/postInstallPackage.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+#When user adds plugin/theme via composer require, check which plugin/theme was added
+#and copy it to core
 if [ -d "contrib/plugins" ] && [ -d "wp/wp-content/plugins" ]; then
   cd contrib/plugins
   installedPlugin=$(ls -ltr | tail -1 | grep -o '[^ ]*$')
   cd ..
   cd ..
   cp -ru contrib/plugins/$installedPlugin wp/wp-content/plugins/
+fi
+
+if [ -d "contrib/themes" ] && [ -d "wp/wp-content/themes" ]; then
+  cd contrib/themes
+  installedTheme=$(ls -ltr | tail -1 | grep -o '[^ ]*$')
+  cd ..
+  cd ..
+  cp -ru contrib/themes/$installedTheme wp/wp-content/themes/
 fi
