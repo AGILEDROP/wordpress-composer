ec6fcd8dac962e2251ee591be557c91325ae512f
Ziga <ziga.koracin@agiledrop.com>
2020-06-08 10:19:22 +0200

    Working on watcher.

--- start.sh
+++ start.sh
@@ -22,3 +22,46 @@ elif [[ "$(uname -s)" == "Darwin" ]]; then
 fi
 
 docker-compose up -d
+
+echo "Pulling image, to watch custom directory changes."
+docker pull coppit/inotify-command
+
+
+if docker ps -a | grep -q 'coppit/inotify-command'; then
+  containerId=$(docker ps -a | grep "coppit/inotify-command" | cut -d ' ' -f1)
+  docker stop $containerId
+  docker rm $containerId
+fi
+
+dirToWatch="/dir/path:"
+dirToWatch+=$(pwd)
+dirToWatch+="/custom/plugins"
+
+sudo docker run --name=inotify-command -d -v /etc/localtime:/etc/localtime -v /config/dir/path:/config:rw -v $dirToWatch coppit/inotify-command
+if [ ! -f "/config/dir/path/custom.conf" ]; then
+  sudo touch /config/dir/path/custom.conf
+fi
+
+watchDir="WATCH_DIR="
+watchDir+=$(pwd)
+watchDir+="/custom/plugins"
+
+echo "Setting watcher for custom directory"
+
+echo "$watchDir" | sudo tee /config/dir/path/custom.conf
+echo -e 'SETTLE_DURATION=5
+        MAX_WAIT_TIME=05:00
+        MIN_PERIOD=10:00
+        COMMAND="touch novaDatoteka.php"
+        USER_ID=0
+        GROUP_ID=0
+        UMASK=0000
+        IGNORE_EVENTS_WHILE_COMMAND_IS_RUNNING=1
+        USE_POLLING=NO
+        DEBUG=0' | sudo tee -a /config/dir/path/custom.conf
+
+if [ -f '/config/dir/path/sample.conf' ]; then
+  sudo rm /config/dir/path/sample.conf
+fi
+
+docker logs inotify-command
